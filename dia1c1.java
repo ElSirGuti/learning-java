105b9253a089de0de02403e23bb6a2de3f75aa55
ElSirGuti <87587118+ElSirGuti@users.noreply.github.com>
2022-10-01 22:35:04 -0400

    Add files via upload

---
+++ dia1c1.java
@@ -0,0 +1,15 @@
+// Primer Día 1-10-22
+
+// Introducción
+// Primera línea siempre, aquí se escribirá todo el programa
+
+// Leyendo de derecha a izquierda nos dice que "dia1c1" es una clase pública, el programa tiene que llevar el nombre de esa clase
+public class dia1c1{
+    // método main: Lo que da inicio al programa (Recuerda identar)
+    // sin ella no funciona el programa
+    public static void main(String args[]) {  
+        // Empezamos con las órdenes SIEMPRE PONER ";" AL TERMINAR UNA LINEA DE INSTRUCCIÓN
+        System.out.println("Hola mundo");
+    }
+}
+
