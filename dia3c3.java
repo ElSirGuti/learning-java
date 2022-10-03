d1c8c5b7ae8a7eb060c9a6d7efc985a669f6e7db
ElSirGuti <87587118+ElSirGuti@users.noreply.github.com>
2022-10-03 01:20:26 -0400

    Add files via upload

---
+++ dia3c3.java
@@ -0,0 +1,23 @@
+// Tercer Día Clase 2 3-10-22
+
+// Ciclos o Bucles (Estructura repetitiva while)
+
+// Estructura repetitiva while
+// var = <valor>;
+// while(condición){
+//     Instrucción;
+// }
+
+// Ejemplo: Realizar un programa que imprima la siguiente serie numeríca: 1, 3, 5, 7, 9,
+
+public class dia3c3{
+    public static void main(String args[]){
+        int i = 1;
+
+        while(i<10){
+            System.out.print(i + ", ");
+            i+=2;
+        }
+
+    }
+}
