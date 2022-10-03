c26441bd5f4829dac2f9bdc5aee2c16124141d85
ElSirGuti <87587118+ElSirGuti@users.noreply.github.com>
2022-10-03 01:11:06 -0400

    Add files via upload

---
+++ dia3c2.java
@@ -0,0 +1,25 @@
+// Tercer Día Clase 1 3-10-22
+
+// Ciclos o Bucles (Estructura repetitiva for)
+
+// Condición: Dar 5 vueltas
+// Instrucción: Dar un salto
+// Contador: 0
+
+// Estructura repetitiva for
+// for(inicio; condición; incremento){
+//      Instrucción;
+// }
+
+// Ejemplo: Realizar un programa que imprima la siguiente serie numeríca: 1, 2, 3, 4, 5,
+
+public class dia3c2 {
+    public static void main(String args[]){
+        for(int i = 1; i <=  5; i++){
+            //(++ significa que incremente de uno en uno y -- lo opuesto)
+            //(para que incremente mas o menos usa i+=N y i-=N respectivamente)
+        
+            System.out.print(i + ", ");
+        }
+    }
+}
